ae3f312674d5c5eaf91e9c94098ec0bf2fac75a4
chedjou03 <chedjou03@yahoo.fr>
2018-12-09 17:32:29 -0500

    initial commit

---
+++ src/main/java/com/springboot/demo/myFirstSpringBootapp/MyFirstSpringBootappApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.demo.myFirstSpringBootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSpringBootappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSpringBootappApplication.class, args);
+	}
+}
---
+++ src/main/java/com/springboot/demo/myFirstSpringBootapp/rest/MyRestController.java
@@ -0,0 +1,21 @@
+package com.springboot.demo.myFirstSpringBootapp.rest;
+
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyRestController {
+	
+	@Value("${coach.name}")
+	private String coachName;
+	@GetMapping("/")
+	public String sayHello()
+	{
+		return "Hello World coach name is: "+coachName+" Time is "+LocalDateTime.now();
+	}
+
+}
